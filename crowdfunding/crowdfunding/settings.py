2462f669cf8c2c7cb920c9f2ab4e54a206498d11
Bolena0905 <anagomez017@gmail.com>
2022-04-21 17:59:18 +0800

    update Proctile req for deploy

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-# import dj_database_url
+import dj_database_url
 import os
 
 
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-6f5fiv53l$d=%d_0_8&znvd!6&d3rfy-qowzswx^u)i-p_dsm6' CHANGE SECRETKEY
-SECRET_KEY = os.environ.get(
-    'DJANGO_SECRET_KEY',
-    '6f5fiv53l$d=%d_0_8&znvd!6&d3rfy-qowzswx^ui-p_dsm6'
+SECRET_KEY = os.environ.get( 
+    'DJANGO_SECRET_KEY', 
+    '5*15pt5log&-bjpkqo0117!b!x4do-mgmxvg8n$3016384zz(7' 
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -108,8 +108,8 @@ DATABASES = {
     }
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
